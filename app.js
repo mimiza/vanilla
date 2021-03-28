d9f1d76c90705ec8c90ed9bb1dca2575a48886fe
MIMIZA <dev@mimiza.com>
2021-03-29 01:03:05 +0700

    update

--- app.js
+++ app.js
@@ -8,10 +8,6 @@ document.body.innerHTML = `
 // load eruda devtools
 document.addEventListener("DOMContentLoaded", e => {
     if (/eruda=true/.test(window.location)) {
-        // const eruda = document.createElement("script")
-        // eruda.src = "//cdn.jsdelivr.net/npm/eruda"
-        // document.body.append(eruda)
-        // setTimeout(() => window.eruda.init(), 3000)
         fetch("//cdn.jsdelivr.net/npm/eruda")
             .then(res => res.text())
             .then(src => {
