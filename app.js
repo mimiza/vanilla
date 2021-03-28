33ec8db972405a50e1a3334fba769744702d920b
MIMIZA <dev@mimiza.com>
2021-03-29 00:09:44 +0700

    update

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-import '/framework.js'
-import '/layouts/simple.js'
+import '/core/framework.js'
+import '/core/layouts/simple.js'
 
 document.body.innerHTML = `
     <simple-layout/>
 `
+
+// load eruda devtools
+document.addEventListener("DOMContentLoaded", (e) => {
+    if (/eruda=true/.test(window.location)) {
+        const el = document.createElement('script')
+        eruda.src = '//cdn.jsdelivr.net/npm/eruda'
+        document.body.append(el)
+        const init = document.createElement('script').innerHTML = 'eruda.init()'
+        document.body.append(init)
+    }
+})
