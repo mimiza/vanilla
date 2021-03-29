17b349e4c0f83b51dce1e82c4e704787ee9dc787
MIMIZA <dev@mimiza.com>
2021-03-29 12:57:05 +0700

    add gun as git submodule

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 import "/core/framework.js"
 import "/core/layouts/simple.js"
 
-document.body.innerHTML = `
-    <simple-layout/>
-`
+// load eruda devtools for mobile browsers
+if (/eruda=true/.test(window.location) || localStorage.getItem('eruda') === 'true') {
+    localStorage.setItem('eruda', true)
+    fetch("//cdn.jsdelivr.net/npm/eruda")
+    .then(res => res.text())
+    .then(src => {
+        eval(src)
+        eruda.init()
+    })
+}
 
-// load eruda devtools
-document.addEventListener("DOMContentLoaded", e => {
-    if (/eruda=true/.test(window.location)) {
-        fetch("//cdn.jsdelivr.net/npm/eruda")
-            .then(res => res.text())
-            .then(src => {
-                eval(src)
-                eruda.init()
-            })
-    }
-})
+document.body.innerHTML = `
+<simple-layout/>
+`
