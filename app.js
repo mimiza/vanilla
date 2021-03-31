79a1f1e84e2d6804d436b795b6a839e0c7cba390
MIMIZA <dev@mimiza.com>
2021-03-31 22:49:46 +0700

    update

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-import {
-    context,
-    setContext
-} from "/core/framework.js"
+import { context, setContext } from "/core/framework.js"
 import "/core/layouts/simple.js"
 
 // load eruda devtools for mobile browsers
-if (/eruda=true/.test(window.location) || localStorage.getItem('eruda') === 'true') {
-    localStorage.setItem('eruda', true)
+if (
+    /eruda=true/.test(window.location) ||
+    localStorage.getItem("eruda") === "true"
+) {
+    localStorage.setItem("eruda", true)
     fetch("//cdn.jsdelivr.net/npm/eruda")
-    .then(res => res.text())
-    .then(src => {
-        eval(src)
-        eruda.init()
-    })
+        .then(res => res.text())
+        .then(src => {
+            eval(src)
+            eruda.init()
+        })
 }
 
-document.body.append(`<simple-layout>hello world</simple-layout>`.html())
+document.body.innerHTML = `<simple-layout>hello world</simple-layout>`
 
 window.setContext = setContext
